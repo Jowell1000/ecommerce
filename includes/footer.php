5bc47911e0d02d59b30ba33e79618aae1d4c39bf
JoWell <joelbahati1000@gmail.com>
2023-03-21 15:30:29 +0300

    lines of code added to footer

---
+++ includes/footer.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce_footer</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <footer>
+        <!-- Small description -->
+        <div class="descript">
+            <p>At <b>GamentFashion Store</b> , we believe in providing our customers with exceptional service. From fast shipping to hassle-free returns, we strive to make your shopping experience as seamless as possible.</p>
+        </div>
+
+        <!-- Get in touch -->
+        <div class="socio">
+
+            <div class="contact">
+                <a href="https://web.whatsapp.com/"> <img src="../images/icons/whatsapp (1).png" alt=""> </a>
+                <a href="https://mail.google.com/mail/u/0/#inbox"> <img src="../images/icons/email.png" alt=""> </a>
+                <p>Concact Us</p>
+            </div>
+            <!-- Separating bar -->
+            <div class="bare"> </div>
+            <!-- Social media icond -->
+            <div class="sMedia">
+                <div class="spacing">.</div>
+                <p>GamentFashion Store</p>
+                <!-- Instagram -->
+                <a href="https://www.instagram.com/renewit1220/"> <img src="../images/icons/instagram.png" alt="" id="instagram"> </a>
+
+                <!-- Facebook -->
+                <a href="#"> <img src="../images/icons/facebook.png" alt="" id="instagram"> </a>
+
+                <!-- twitter -->
+                <a href="https://www.instagram.com/renewit1220/"> <img src="../images/icons/twitter.png" alt="" id="instagram"> </a>
+            </div>
+
+        </div>
+
+        <!-- -- Copyright @ -->
+
+        <div class="copyright">
+            copyright &copy; <?php echo date('Y') ?>GarmentFashion Store 
+        </div>
+
+    </footer>
+</body>
+</html>
